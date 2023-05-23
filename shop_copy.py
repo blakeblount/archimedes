c6e1d63ed75e42b0ea3dda7d412be2004da7887c
Blake Blount <bblount@sefcor.com>
2023-05-23 14:10:28 -0400

    Add try statement to print method

--- shop_copy.py
+++ shop_copy.py
@@ -117,63 +117,69 @@ class ShopCopy:
                 break
             img = img[0]
 
-            # This line is what runs OCR on the part drawing.
-            ocr_data = pytesseract.image_to_data(img, output_type='dict')
+            try:
 
-            # These are the strings we are looking for on the drawings.
-            job_str = "Job"
-            item_str = "Item(s)"
-            qty_str = "Qty."
+                # This line is what runs OCR on the part drawing.
+                ocr_data = pytesseract.image_to_data(img, output_type='dict')
 
-            # The OCR returns a dictionary with headings for the word found, and then location, height, and width in
-            # pixels.
-            # Establish Job block position
-            for i, text in enumerate(ocr_data['text']):
-                if job_str in text:
-                    print(f"Found {job_str} at {i}")
-                    job_left = ocr_data['left'][i]
-                    job_top = ocr_data['top'][i]
-                    job_width = ocr_data['width'][i]
-                    job_height = ocr_data['height'][i]
+                # These are the strings we are looking for on the drawings.
+                job_str = "Job"
+                item_str = "Item(s)"
+                qty_str = "Qty."
 
-            # Establish Item block position
-            for i, text in enumerate(ocr_data['text']):
-                if item_str in text:
-                    print(f"Found {item_str} at {i}")
-                    item_left = ocr_data['left'][i]
-                    item_top = ocr_data['top'][i]
-                    item_width = ocr_data['width'][i]
-                    item_height = ocr_data['height'][i]
+                job_left, job_top, job_width, job_height = None, None, None, None
+                item_left, item_top, item_width, item_height = None, None, None, None
+                qty_left, qty_top, qty_width, qty_height = None, None, None, None
 
-            # Establish Quantity block position
-            for i, text in enumerate(ocr_data['text']):
-                if qty_str in text:
-                    print(f"Found {qty_str} at {i}")
-                    qty_left = ocr_data['left'][i]
-                    qty_top = ocr_data['top'][i]
-                    qty_width = ocr_data['width'][i]
-                    qty_height = ocr_data['height'][i]
+                # The OCR returns a dictionary with headings for the word found, and then location, height, and width in
+                # pixels.
+                # Establish Job block position
+                for i, text in enumerate(ocr_data['text']):
+                    if job_str in text:
+                        job_left = ocr_data['left'][i]
+                        job_top = ocr_data['top'][i]
+                        job_width = ocr_data['width'][i]
+                        job_height = ocr_data['height'][i]
 
-            # Create object on which to write data
-            draw = ImageDraw.Draw(img)
-            font = ImageFont.truetype("arial.ttf", size=30)
+                # Establish Item block position
+                for i, text in enumerate(ocr_data['text']):
+                    if item_str in text:
+                        item_left = ocr_data['left'][i]
+                        item_top = ocr_data['top'][i]
+                        item_width = ocr_data['width'][i]
+                        item_height = ocr_data['height'][i]
 
-            # Determine start points of text to be inserted. Determining these x and y coordinates just took 
-            # trial-and-error. Ideally, we will test for drawing type (A, B, or C), and have specific values
-            # optimized for each type. For now, these work well enough for all three.
-            job_x = job_left + (3 * job_width) + 20
-            job_y = job_top - (job_height / 2)
+                # Establish Quantity block position
+                for i, text in enumerate(ocr_data['text']):
+                    if qty_str in text:
+                        qty_left = ocr_data['left'][i]
+                        qty_top = ocr_data['top'][i]
+                        qty_width = ocr_data['width'][i]
+                        qty_height = ocr_data['height'][i]
 
-            item_x = job_left + (3 * job_width) + 20
-            item_y = item_top - (job_height / 2)
+                # Create object on which to write data
+                draw = ImageDraw.Draw(img)
+                font = ImageFont.truetype("arial.ttf", size=30)
 
-            qty_x = job_left + (3 * job_width) + 20
-            qty_y = qty_top - (job_height / 2)
+                # Determine start points of text to be inserted. Determining these x and y coordinates just took 
+                # trial-and-error. Ideally, we will test for drawing type (A, B, or C), and have specific values
+                # optimized for each type. For now, these work well enough for all three.
+                job_x = job_left + (3 * job_width) + 20
+                job_y = job_top - (job_height / 2)
 
-            # Draw text on the shop copy drawing
-            draw.text((job_x, job_y), job_text, font=font, fill=(0, 0, 0))
-            draw.text((item_x, item_y), item_text, font=font, fill=(0, 0, 0))
-            draw.text((qty_x, qty_y), qty_text, font=font, fill=(0, 0, 0))
+                item_x = job_left + (3 * job_width) + 20
+                item_y = item_top - (job_height / 2)
+
+                qty_x = job_left + (3 * job_width) + 20
+                qty_y = qty_top - (job_height / 2)
+
+                # Draw text on the shop copy drawing
+                draw.text((job_x, job_y), job_text, font=font, fill=(0, 0, 0))
+                draw.text((item_x, item_y), item_text, font=font, fill=(0, 0, 0))
+                draw.text((qty_x, qty_y), qty_text, font=font, fill=(0, 0, 0))
+
+            except Exception as e:
+                print(f"Failed to process drawing {drawing_filename}. Error: {str(e)}")
 
 
             # The below is a less-than-ideal way of saving but I was running into an issue saving directly as a PDF.
